cad162ebc1f6a70ec16b6e528b1865ef19cd86aa
sujunghwang <64738942+sujunghwang@users.noreply.github.com>
2021-06-19 21:09:36 +0900

    Rename business_journal.sql to BUSINESS_JOURNAL.sql

---
+++ BUSINESS_JOURNAL.sql
@@ -0,0 +1,107 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 토요일-6월-19-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table BUSINESS_JOURNAL
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."BUSINESS_JOURNAL" 
+   (	"거래처" VARCHAR2(34 BYTE), 
+	"물품분류" VARCHAR2(26 BYTE), 
+	"총거래비용" NUMBER(38,0)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into SYSTEM.BUSINESS_JOURNAL
+SET DEFINE OFF;
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','A1',420400);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','A2',1309900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','A3',1784200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','AA',1268200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','AAA',557900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','C1',806300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','C2',258700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','B1',1726900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','B2',598700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','R',289300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','E1',1568900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','E2',1278400);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','H1',767500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','H2',1504000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','H3',802300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','G1',90000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','G2',252700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','G22',1886800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','G3',49900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Q1',543900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Q2',850700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Q4',1363100);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Q3',542900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Y1',405200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','Y2',1497500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','PY1',152800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','PY2',72600);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','S',295600);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','S1',248000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','S2',49900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','M',1234800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','T2',1191800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','T1',472000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('(주)민성폐차산업','FL',593900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','A1',185300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','A2',986500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','A3',1539700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','AA',645800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','AAA',65300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','C1',737300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','C2',569300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','B1',295700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','B2',1459300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','R',1185800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','E1',826700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','E2',1327700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','H1',1587700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','H2',65300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','H3',552500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','G1',967700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','G2',1345300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','G22',1793200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','G3',441000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Q1',879500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Q2',1552500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Q4',824800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Q3',213800);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Y1',2478200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','Y2',1332500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','PY1',227300);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','PY2',118900);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','S',309600);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','S1',248000);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','S2',421700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','M',399700);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','T2',2533200);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','T1',594500);
+Insert into SYSTEM.BUSINESS_JOURNAL ("거래처","물품분류","총거래비용") values ('주식회사 월드리싸이클링','FL',1284500);
+--------------------------------------------------------
+--  DDL for Index SYS_C0011165
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "SYSTEM"."SYS_C0011165" ON "SYSTEM"."BUSINESS_JOURNAL" ("거래처", "물품분류") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  Constraints for Table BUSINESS_JOURNAL
+--------------------------------------------------------
+
+  ALTER TABLE "SYSTEM"."BUSINESS_JOURNAL" ADD PRIMARY KEY ("거래처", "물품분류")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM"  ENABLE;
